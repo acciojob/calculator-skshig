f79092d72b136ef13db988284c274d486dca3cf7
Sakshi Gupta <43090350+skshig@users.noreply.github.com>
2023-09-07 21:37:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 //your code here
+
+let one = document.getElementById('block1');
+let two = document.getElementById('block2');
+let three = document.getElementById('block3');
+let four = document.getElementById('block4');
+let five = document.getElementById('block5');
+let six = document.getElementById('block6');
+let seven = document.getElementById('block7');
+let eight = document.getElementById('block8');
+let nine = document.getElementById('block9');
+let zero = document.getElementById('block0');
+
+let plus = document.getElementById('plus');
+let minus = document.getElementById('minus');
+let multiply = document.getElementById('multiply');
+let divide = document.getElementById('divide');
+let clear = document.getElementById('clear');
+let dot = document.getElementById('dot');
+let ans = document.getElementById('ans');
+let reset = document.getElementById('clr');
+
+function add(n) {
+	return 
+}
